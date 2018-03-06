50d7201fd251565b1faa54b436da9a0814f0781c
Christos Nasikas <xristosnasikas@gmail.com>
2018-03-06 02:06:56 +0200

    Change theme colors

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,31 @@
-  import React from 'react'
-  import AppRoot from './components/Root'
+import React from 'react'
+import AppRoot from './components/Root'
 
-  import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
-  import purple from 'material-ui/colors/purple'
-  import teal from 'material-ui/colors/teal'
-  import red from 'material-ui/colors/red'
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
 
-  const theme = createMuiTheme({
-    palette: {
-      primary: purple, // Purple and green play nicely together.
-      secondary: teal,
-      error: red
-    }
-  })
-
-  export default class App extends React.Component {
-    render () {
-      return (
-        <MuiThemeProvider theme={theme}>
-          <AppRoot />
-        </MuiThemeProvider>
-      )
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000'
     }
   }
+})
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <AppRoot />
+      </MuiThemeProvider>
+    )
+  }
+}
