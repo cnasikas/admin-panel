a6d1a9105fd3c8d3794721ce28fb6f7abb8090e5
Christos Nasikas <xristosnasikas@gmail.com>
2018-04-21 22:49:26 +0300

    Make api base url relative to domain

--- app/src/index.js
+++ app/src/index.js
@@ -15,7 +15,7 @@ import App from './App'
 import './index.css'
 
 const client = axios.create({
-  baseURL: 'http://localhost/admin.dev/api/public/',
+  baseURL: '/api',
   responseType: 'json'
 })
 
